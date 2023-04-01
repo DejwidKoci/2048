a053e284d911fec2df1b496301dd9c84643483ec
Dejwid <s205915@sggw.edu.pl>
2023-04-01 18:21:47 +0200

    game_over()

--- 2048.py
+++ 2048.py
@@ -147,6 +147,28 @@ class Game:
                     return True
         return False
 
+    # Check if game is over (win/lose)
+    def game_over(self):
+        if any(2048 in row for row in self.matrix):
+            game_over_frame = tk.Frame(self.main_grid, borderwidth = 2)
+            game_over_frame.place(relx = 0.5, rely = 0.5, anchor = "center")
+            tk.Label(
+                game_over_frame,
+                text = "You win!",
+                bg = c.WINNER_BG,
+                fg = c.GAME_OVER_FONT_COLOR,
+                font = c.GAME_OVER_FONT
+            ).pack()
+        elif not any (0 in row for row in self.matrix) and not self.horizontal_move_exists() and not self.vertical_move_exists():
+            game_over_frame = tk.Frame(self.main_grid, borderwidth = 2)
+            game_over_frame.place(relx = 0.5, rely = 0.5, anchor = "center")
+            tk.Label(
+                game_over_frame,
+                text = "Game over!",
+                bg = c.LOSER_BG,
+                fg = c.GAME_OVER_FONT_COLOR,
+                font = c.GAME_OVER_FONT
+            ).pack()
 
 
     def run(self):
